f67b0b2cd7737b1be73b781ce75eaaa932f391fa
Van4xx <van200529ter@gmail.com>
2024-10-20 19:57:57 +0300

    change errors

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { useTelegram } from '../../hooks/useTelegram';
 
 const Header = () => {
 
-    const {user, onClose} = useTelegram
+    const {user, onClose} = useTelegram();
 
     return (
         <div className='header'>
