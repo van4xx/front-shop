5fe18721c18d6f1f445d61bd1578df27d8c33dfa
Van4xx <van200529ter@gmail.com>
2024-10-20 19:42:54 +0300

    add hooks and toggleButton change errors 2

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,16 +1,16 @@
-const tg = window.Telegram.WebApp;
+const tg = window.Telegram?.WebApp;
 
 export function useTelegram(){
 
     const onClose = () => {
-        tg.close()
+        tg?.close()
     }
 
     const onToggleButton = () => {
-        if(tg.MainButton.isVisible){
+        if(tg?.MainButton.isVisible){
             tg.MainButton.hide();
         } else {
-            tg.MainButton.show();
+            tg?.MainButton.show();
         }
     }
 
@@ -18,7 +18,6 @@ export function useTelegram(){
         onClose,
         onToggleButton,
         tg,
-        user: tg.initDataUnsafe?.user,
-
+        user: tg?.initDataUnsafe?.user,
     }
 }
