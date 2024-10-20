fef29dc4d3df9bc4d78f35446e0348277676bb45
Van4xx <van200529ter@gmail.com>
2024-10-20 20:29:45 +0300

    Add header component in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
+import Header from './components/Header/Header';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <div className="App">
+        <Header />
         <button onClick={onToggleButton}>toggle</button>
     </div>
   );
